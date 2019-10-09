1f19b227d1404a515d9e6c1cd6069b15a87f837b
Nurali Virani <nurali.techie@gmail.com>
2019-10-09 12:08:28 +0530

    Update wait/member.go
    Co-Authored-By: Matous Jobanek <matousjobanek@gmail.com>

--- wait/member.go
+++ wait/member.go
@@ -107,6 +107,7 @@ func (a *MemberAwaitility) WaitForNamespace(username, typeName, revision string)
 			a.T.Logf("waiting for availability of Namespace type '%s' with revision '%s'", typeName, revision)
 			return false, nil
 		}
+		require.Len(a.T, namespaceList.Items, 1, "there should be only one Namespace found")
 		a.T.Logf("found Namespace type '%s' with revision '%s'", typeName, revision)
 		return true, nil
 	})
