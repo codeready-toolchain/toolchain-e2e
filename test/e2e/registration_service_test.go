563467bf4c0dc459366a02e04e264965d15c61ca
tinakurian <tkurian@redhat.com>
2019-11-19 09:38:00 -0500

    PR comments

--- test/e2e/registration_service_test.go
+++ test/e2e/registration_service_test.go
@@ -44,14 +44,8 @@ func (s *registrationServiceTestSuite) TestLandingPageReachable() {
 	// just make sure that the landing page is reachable
 	req, err := http.NewRequest("GET", s.route, nil)
 	require.NoError(s.T(), err)
-	client := &http.Client{
-		Timeout: time.Second * 10,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
+	client := getClient()
+
 	resp, err := client.Do(req)
 	require.NoError(s.T(), err)
 	assert.Equal(s.T(), http.StatusOK, resp.StatusCode)
@@ -62,14 +56,7 @@ func (s *registrationServiceTestSuite) TestHealth() {
 		// Call health endpoint.
 		req, err := http.NewRequest("GET", s.route+"/api/v1/health", nil)
 		require.NoError(s.T(), err)
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -83,13 +70,15 @@ func (s *registrationServiceTestSuite) TestHealth() {
 
 		mp := make(map[string]interface{})
 		err = json.Unmarshal([]byte(body), &mp)
-		require.Nil(s.T(), err)
+		require.NoError(s.T(), err)
 
 		// Verify JSON response.
 		alive := mp["alive"]
+		require.IsType(s.T(), alive, true)
 		require.True(s.T(), alive.(bool))
 
 		environment := mp["environment"]
+		require.IsType(s.T(), environment, "")
 		require.Equal(s.T(), "e2e-tests", environment.(string))
 
 		revision := mp["revision"]
@@ -108,14 +97,7 @@ func (s *registrationServiceTestSuite) TestAuthConfig() {
 		// Call authconfig endpoint.
 		req, err := http.NewRequest("GET", s.route+"/api/v1/authconfig", nil)
 		require.NoError(s.T(), err)
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -124,35 +106,42 @@ func (s *registrationServiceTestSuite) TestAuthConfig() {
 		defer resp.Body.Close()
 
 		body, err := ioutil.ReadAll(resp.Body)
-		require.Nil(s.T(), err)
+		require.NoError(s.T(), err)
 		require.NotNil(s.T(), body)
 
 		mp := make(map[string]interface{})
 		err = json.Unmarshal([]byte(body), &mp)
-		require.Nil(s.T(), err)
+		require.NoError(s.T(), err)
 
 		// Verify JSON response.
-		alive := mp["auth-client-library-url"]
-		require.Equal(s.T(), alive.(string), "https://sso.prod-preview.openshift.io/auth/js/keycloak.js")
+		authClientLibraryURL := mp["auth-client-library-url"]
+		require.IsType(s.T(), authClientLibraryURL, "")
+		require.Equal(s.T(), authClientLibraryURL.(string), "https://sso.prod-preview.openshift.io/auth/js/keycloak.js")
 
-		testingMode := mp["auth-client-config"].(string)
+		authClientConfig := mp["auth-client-config"]
+		require.IsType(s.T(), authClientConfig, "")
 		mp1 := make(map[string]interface{})
-		err = json.Unmarshal([]byte(testingMode), &mp1)
-		require.Nil(s.T(), err)
+		err = json.Unmarshal([]byte(authClientConfig.(string)), &mp1)
+		require.NoError(s.T(), err)
 
 		realm := mp1["realm"]
+		require.IsType(s.T(), realm, "")
 		require.Equal(s.T(), realm.(string), "toolchain-public")
 
 		authServerURL := mp1["auth-server-url"]
+		require.IsType(s.T(), authServerURL, "")
 		require.Equal(s.T(), authServerURL.(string), "https://sso.prod-preview.openshift.io/auth")
 
 		sslRequired := mp1["ssl-required"]
+		require.IsType(s.T(), sslRequired, "")
 		require.Equal(s.T(), sslRequired.(string), "none")
 
 		resource := mp1["resource"]
+		require.IsType(s.T(), resource, "")
 		require.Equal(s.T(), resource.(string), "crt")
 
 		publicClient := mp1["public-client"]
+		require.IsType(s.T(), publicClient, true)
 		require.True(s.T(), publicClient.(bool))
 	})
 }
@@ -173,14 +162,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		req, err := http.NewRequest("POST", s.route+"/api/v1/signup", bytes.NewBuffer(requestBody))
 		require.NoError(s.T(), err)
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -199,8 +181,9 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 
 		// Check token error.
-		tokenErr := mp["error"].(string)
-		require.Equal(s.T(), "no token found", tokenErr)
+		tokenErr := mp["error"]
+		require.IsType(s.T(), tokenErr, "")
+		require.Equal(s.T(), "no token found", tokenErr.(string))
 	})
 
 	s.Run("verify_post_signup_error_unknown_auth_header", func() {
@@ -209,14 +192,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "1223123123")
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -235,8 +211,9 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 
 		// Check token error.
-		tokenErr := mp["error"].(string)
-		require.Equal(s.T(), "found unknown authorization header:1223123123", tokenErr)
+		tokenErr := mp["error"]
+		require.IsType(s.T(), tokenErr, "")
+		require.Equal(s.T(), "found unknown authorization header:1223123123", tokenErr.(string))
 	})
 
 	s.Run("verify_post_signup_error_invalid_token", func() {
@@ -245,14 +222,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "Bearer 1223123123")
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -271,8 +241,9 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 
 		// Check token error.
-		tokenErr := mp["error"].(string)
-		require.Equal(s.T(), "token contains an invalid number of segments", tokenErr)
+		tokenErr := mp["error"]
+		require.IsType(s.T(), tokenErr, "")
+		require.Equal(s.T(), "token contains an invalid number of segments", tokenErr.(string))
 	})
 
 	s.Run("verify_post_signup_valid_token", func() {
@@ -281,14 +252,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "Bearer "+token0)
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -306,14 +270,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		req, err := http.NewRequest("GET", s.route+"/api/v1/signup", nil)
 		require.NoError(s.T(), err)
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -332,8 +289,9 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 
 		// Check token error.
-		tokenErr := mp["error"].(string)
-		require.Equal(s.T(), "no token found", tokenErr)
+		tokenErr := mp["error"]
+		require.IsType(s.T(), tokenErr, "")
+		require.Equal(s.T(), "no token found", tokenErr.(string))
 	})
 	s.Run("verify_get_signup_error_unknown_auth_header", func() {
 		// Call signup endpoint with invalid authroization header.
@@ -341,14 +299,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "1223123123")
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -367,8 +318,9 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 
 		// Check token error.
-		tokenErr := mp["error"].(string)
-		require.Equal(s.T(), "found unknown authorization header:1223123123", tokenErr)
+		tokenErr := mp["error"]
+		require.IsType(s.T(), tokenErr, "")
+		require.Equal(s.T(), "found unknown authorization header:1223123123", tokenErr.(string))
 	})
 	s.Run("verify_get_signup_error_invalid_token", func() {
 		// Call signup endpoint with an invalid token.
@@ -376,14 +328,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "Bearer 1223123123")
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -402,8 +347,9 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 
 		// Check token error.
-		tokenErr := mp["error"].(string)
-		require.Equal(s.T(), "token contains an invalid number of segments", tokenErr)
+		tokenErr := mp["error"]
+		require.IsType(s.T(), tokenErr, "")
+		require.Equal(s.T(), "token contains an invalid number of segments", tokenErr.(string))
 	})
 	s.Run("verify_get_signup_valid_token_status_200OK", func() {
 		// Call signup endpoint with an valid token.
@@ -411,14 +357,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "Bearer "+token0)
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -447,14 +386,7 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		req.Header.Set("Authorization", "Bearer "+token1)
 		req.Header.Set("content-type", "application/json")
-		client := &http.Client{
-			Timeout: time.Second * 10,
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
+		client := getClient()
 
 		resp, err := client.Do(req)
 		require.NoError(s.T(), err)
@@ -465,6 +397,22 @@ func (s *registrationServiceTestSuite) TestSignup() {
 		require.NoError(s.T(), err)
 		require.NotNil(s.T(), body)
 
+		mp := make(map[string]interface{})
+		err = json.Unmarshal([]byte(body), &mp)
+		require.NoError(s.T(), err)
+
 		assert.Equal(s.T(), http.StatusNotFound, resp.StatusCode)
 	})
+}
+
+// getClient create's a new client.
+func getClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Second * 10,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
 }
