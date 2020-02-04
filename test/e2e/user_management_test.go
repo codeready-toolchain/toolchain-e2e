1168c19c11c51867acc109d3fa254f8bcaaca55c
tinakurian <tkurian@redhat.com>
2020-02-04 10:15:32 -0500

    tests

--- test/e2e/user_management_test.go
+++ test/e2e/user_management_test.go
@@ -1,16 +1,15 @@
 package e2e
 
 import (
-	"context"
-	"github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
-	authsupport "github.com/codeready-toolchain/toolchain-common/pkg/test/auth"
-	"github.com/codeready-toolchain/toolchain-e2e/testsupport"
-	"github.com/codeready-toolchain/toolchain-e2e/wait"
-	framework "github.com/operator-framework/operator-sdk/pkg/test"
-	uuid "github.com/satori/go.uuid"
+	userv1 "github.com/openshift/api/user/v1"
 	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/types"
 	"testing"
+
+	"github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
+	"github.com/codeready-toolchain/toolchain-e2e/testsupport"
+	"github.com/codeready-toolchain/toolchain-e2e/wait"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
 
 	"github.com/stretchr/testify/suite"
 )
@@ -31,44 +30,44 @@ type userManagementTestSuite struct {
 
 
 func (s *userManagementTestSuite) SetupSuite() {
-	userAccountList := &v1alpha1.UserAccountList{}
-	s.testCtx, s.awaitility = testsupport.WaitForDeployments(s.T(), userAccountList)
+	userSignupList := &v1alpha1.UserSignupList{}
+	s.testCtx, s.awaitility = testsupport.WaitForDeployments(s.T(), userSignupList)
 	s.hostAwait = s.awaitility.Host()
 	s.memberAwait = s.awaitility.Member()
-	s.namespace = s.awaitility.MemberNs
+	s.namespace = s.awaitility.HostNs
 }
 
 func (s *userManagementTestSuite) TearDownTest() {
 	s.testCtx.Cleanup()
 }
 
-// TINA
 func (s *userManagementTestSuite) TestUserDisabled() {
-	testUserIdentity := &authsupport.Identity{
-		ID:       uuid.NewV4(),
-		Username: "janedoe",
-	}
+	require.True(s.T(), true)
+	//testUserIdentity := &authsupport.Identity{
+	//	ID:       uuid.NewV4(),
+	//	Username: "janedoe",
+	//}
+
+	userSignup := createAndApproveSignup(s.T(), s.awaitility, "janedoetina")
 
 	// Post signUp
-	postSignup(s.T(), s.awaitility.RegistrationServiceURL, *testUserIdentity)
-	userSignup, err := s.awaitility.Host().WaitForUserSignup(testUserIdentity.ID.String(), wait.UntilUserSignupHasConditions(pendingApproval()...))
-	require.NoError(s.T(), err)
+	//postSignup(s.T(), s.awaitility.RegistrationServiceURL, *testUserIdentity)
+	//userSignup, err := s.awaitility.Host().WaitForUserSignup(testUserIdentity.ID.String(), wait.UntilUserSignupHasConditions(pendingApproval()...))
+	//require.NoError(s.T(), err)
 
 	// Approve signUp
-	userSignup.Spec.Approved = true
-	err = s.awaitility.Host().Client.Update(context.TODO(), userSignup)
-	require.NoError(s.T(), err)
-
-	userSignup, err = s.awaitility.Host().WaitForUserSignup(userSignup.Name, wait.UntilUserSignupHasConditions(approvedByAdmin()...))
-	require.NoError(s.T(), err)
+	//userSignup.Spec.Approved = true
+	//err = s.awaitility.Host().Client.Update(context.TODO(), userSignup)oc
+	//require.NoError(s.T(), err)
 
 	// Get MasterUserRecord
-	mur, err := s.hostAwait.WaitForMasterUserRecord(userSignup.Status.CompliantUsername)
+	mur, err := s.hostAwait.WaitForMasterUserRecord(userSignup.Spec.Username)
 	require.NoError(s.T(), err)
 
 	// Get the UserAccount
 	userAccount, err := s.memberAwait.WaitForUserAccount(mur.Name,
-		wait.UntilUserAccountHasConditions(provisioned()))
+		wait.UntilUserAccountHasConditions(provisioned()),
+		wait.UntilUserAccountHasSpec(mur.Spec.UserAccounts[0].Spec))
 	require.NoError(s.T(), err)
 	require.NotNil(s.T(), userAccount)
 
@@ -80,53 +79,36 @@ func (s *userManagementTestSuite) TestUserDisabled() {
 	_, err = s.memberAwait.WaitForIdentity(toIdentityName(userAccount.Spec.UserID))
 	require.NoError(s.T(), err)
 
-	// Tina TODO: Wait for this instead
-	tmplTier := &v1alpha1.NSTemplateTier{}
-	err = s.awaitility.Member().Client.Get(context.TODO(), types.NamespacedName{Namespace: s.awaitility.HostNs, Name: "basic"}, tmplTier)
-	require.NoError(s.T(), err)
-
 	// Get the latest UserAccount
-	userAccount, err = s.memberAwait.WaitForUserAccount(mur.Name)
+	userAccount, err = s.memberAwait.WaitForUserAccount(userAccount.Name)
 	require.NoError(s.T(), err)
 	require.NotNil(s.T(), userAccount)
 
 	// Disable account
-	userAccount.Spec.Disabled = true
-	err = s.awaitility.Client.Update(context.TODO(), userAccount)
+	mur, err = s.hostAwait.WaitForMasterUserRecord(userSignup.Spec.Username)
 	require.NoError(s.T(), err)
 
-	//tmplTier = &v1alpha1.NSTemplateTier{}
-	//err = s.awaitility.Member().Client.Get(context.TODO(), types.NamespacedName{Namespace: s.awaitility.HostNs, Name: "basic"}, tmplTier)
-	//require.NoError(s.T(), err)
+	mur.Spec.Disabled = true
+	err = s.awaitility.Host().Client.Update(context.TODO(), mur)
+	require.NoError(s.T(), err)
 
-	//// Get User - should be not found
-	//err = s.memberAwait.WaitUntilUserDeleted(userAccount.Name)
-	//require.NoError(s.T(), err)
+	// Get the latest UserAccount
+	userAccount, err = s.memberAwait.WaitForUserAccount(userAccount.Name)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), userAccount)
 
-	//// Get Identity - should be not found
-	//err = s.memberAwait.WaitUntilIdentityDeleted(toIdentityName(userAccount.Spec.UserID))
-	//require.NoError(s.T(), err)
+	// Check that the UserAccount is now set to disabled
+	require.True(s.T(), userAccount.Spec.Disabled)
 
-	//// Get the latest UserAccount
-	//userAccount, err = s.memberAwait.WaitForUserAccount(userAccount.Name,
-	//	wait.UntilUserAccountHasConditions(disabled()))
-	//require.NoError(s.T(), err)
-	//require.NotNil(s.T(), userAccount)
-	//
-	//// Disable account
-	//userAccount.Spec.Disabled = false
-	//err = s.awaitility.Member().Client.Update(context.TODO(), userAccount)
-	//require.NoError(s.T(), err)
-	//
 	//// Get User
-	//_, err = s.memberAwait.WaitForUser(userAccount.Name)
-	//require.NoError(s.T(), err)
+	//user := &userv1.User{}
+	//s.awaitility.Client.Get(context.TODO(), types.NamespacedName{Name: userAccount.Namespace}, user)
+	////err = s.memberAwait.WaitUntilUserDeleted(userAccount.Name)
+	//require.Error(s.T(), err)
 	//
 	//// Get Identity
-	//_, err = s.memberAwait.WaitForIdentity(toIdentityName(userAccount.Spec.UserID))
-	//require.NoError(s.T(), err)
-	//
-	//tmplTier = &v1alpha1.NSTemplateTier{}
-	//err = s.awaitility.Member().Client.Get(context.TODO(), types.NamespacedName{Namespace: s.awaitility.HostNs, Name: "basic"}, tmplTier)
-	//require.NoError(s.T(), err)
+	////err = s.memberAwait.WaitUntilIdentityDeleted(toIdentityName(userAccount.Spec.UserID))
+	//identity := &userv1.Identity{}
+	//s.awaitility.Client.Get(context.TODO(), types.NamespacedName{Name: toIdentityName(userAccount.Spec.UserID)}, identity)
+	//require.Error(s.T(), err)
 }
