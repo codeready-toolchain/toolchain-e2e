011f5d34b836d956d9e3cf2d837bd8e51c3bf06c
Matous Jobanek <mjobanek@redhat.com>
2020-03-05 14:19:14 +0100

    fix log message

--- wait/member.go
+++ wait/member.go
@@ -198,7 +198,7 @@ func (a *MemberAwaitility) WaitForNamespace(username, typeName, revision, tier s
 			for _, ns := range allNSs.Items {
 				allNSNames[ns.Name] = ns.Labels
 			}
-			a.T.Logf("waiting for availability of namespace of type '%s' with revision '%s' and owned by '%s'. Currently available codeready-toolchain NSs: '%+v'", typeName, revision, username, allNSNames)
+			a.T.Logf("waiting for availability of namespace of type '%s' with revision '%s', tier '%s' and owned by '%s'. Currently available codeready-toolchain NSs: '%+v'", typeName, revision, tier, username, allNSNames)
 			return false, nil
 		}
 		require.Len(a.T, namespaceList.Items, 1, "there should be only one Namespace found")
