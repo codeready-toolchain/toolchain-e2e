cfee99ff399d64c9176acb796d674f79b67b8cfc
Xavier Coulon <xcoulon@redhat.com>
2020-08-26 11:02:59 +0200

    add makefile goal for perf tests
    also, increase to 1k users instead of 10.
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ make/perf.mk
@@ -0,0 +1,20 @@
+###########################################################
+#
+# Performance Tests
+#
+###########################################################
+
+.PHONY: test-perf
+## Run the performance tests
+test-perf: deploy-e2e perf-run
+	@echo "The tests successfully finished"
+	@echo "To clean the cluster run 'make clean-e2e-resources'"
+
+.PHONY: perf-run
+perf-run:
+	oc get toolchaincluster -n $(HOST_NS)
+	oc get toolchaincluster -n $(MEMBER_NS)
+	-oc new-project $(TEST_NS) --display-name e2e-tests 1>/dev/null
+	MEMBER_NS=${MEMBER_NS} HOST_NS=${HOST_NS} REGISTRATION_SERVICE_NS=${REGISTRATION_SERVICE_NS} operator-sdk test local ./test/perf --no-setup --operator-namespace $(TEST_NS) --verbose --go-test-flags "-timeout=60m -failfast" || \
+	($(MAKE) print-logs HOST_NS=${HOST_NS} MEMBER_NS=${MEMBER_NS} REGISTRATION_SERVICE_NS=${REGISTRATION_SERVICE_NS} && exit 1)
+
