9dc62671e144ecb8bd6e22dd0e18cf43b02cf163
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-07-12 10:52:30 -0400

    Test toolchainconfig and memberoperatorconfig

--- make/test.mk
+++ make/test.mk
@@ -163,13 +163,16 @@ deploy-members: create-member1 create-member2 get-and-publish-member-operator
 create-member1:
 	@echo "Deploying member operator to $(MEMBER_NS)..."
 	$(MAKE) create-project PROJECT_NAME=${MEMBER_NS}
+	-oc label ns ${MEMBER_NS} app=member-operator
 
 .PHONY: create-member2
 create-member2:
 ifeq ($(SECOND_MEMBER_MODE),true)
 	@echo "Deploying member operator to ${MEMBER_NS_2}..."
 	$(MAKE) create-project PROJECT_NAME=${MEMBER_NS_2}
-	oc apply -f deploy/member2-operator/member-operator-config-map.yaml -n ${MEMBER_NS_2}
+	-oc label ns ${MEMBER_NS_2} app=member-operator
+	oc apply -f ${MEMBER_REPO_PATH}/config/crd/bases/toolchain.dev.openshift.com_memberoperatorconfigs.yaml
+	oc apply -f deploy/member2-operator/${ENVIRONMENT}/ -n ${MEMBER_NS_2}
 endif
 
 .PHONY: deploy-host
@@ -179,15 +182,29 @@ deploy-host: create-host-project get-and-publish-host-operator create-host-resou
 create-host-project:
 	@echo "Deploying host operator to ${HOST_NS}..."
 	$(MAKE) create-project PROJECT_NAME=${HOST_NS}
+	-oc label ns ${MEMBER_NS_2} app=host-operator
 
 .PHONY: create-host-resources
 create-host-resources:
-	oc apply -f deploy/host-operator/ -n ${HOST_NS}
+	oc apply -f ${HOST_REPO_PATH}/config/crd/bases/toolchain.dev.openshift.com_toolchainconfigs.yaml
+	oc apply -f deploy/host-operator/${ENVIRONMENT}/ -n ${HOST_NS}
+	# patch toolchainconfig to prevent webhook deploy for 2nd member, a 2nd webhook deploy causes the webhook verification in e2e tests to fail
+	# since e2e environment has 2 member operators running in the same cluster
+	if [[ ${SECOND_MEMBER_MODE} == true ]]; then \
+		API_ENDPOINT=`oc get infrastructure cluster -o jsonpath='{.status.apiServerURL}'`; \
+		TOOLCHAIN_CLUSTER_NAME=`echo "$${API_ENDPOINT}" | sed 's/.*api\.\([^:]*\):.*/\1/'`; \
+		echo "API_ENDPOINT $${API_ENDPOINT}"; \
+		echo "TOOLCHAIN_CLUSTER_NAME $${TOOLCHAIN_CLUSTER_NAME}"; \
+		PATCH_FILE=/tmp/patch-toolchainconfig_${DATE_SUFFIX}.json; \
+		echo "{\"spec\":{\"members\":{\"specificPerMemberCluster\":{\"member-$${TOOLCHAIN_CLUSTER_NAME}2\":{\"webhook\":{\"deploy\":false}}}}}}" > $$PATCH_FILE; \
+		oc patch toolchainconfig config -n $(HOST_NS) --type=merge --patch "$$(cat $$PATCH_FILE)"; \
+	fi;
 
 .PHONY: create-project
 create-project:
 	-oc new-project ${PROJECT_NAME} 1>/dev/null
 	-oc project ${PROJECT_NAME}
+	-oc label ns ${PROJECT_NAME} toolchain.dev.openshift.com/provider=codeready-toolchain
 
 .PHONY: display-eval
 display-eval:
