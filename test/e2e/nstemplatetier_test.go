16c534670b705cbaa2dd1e47831e00b052185513
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-07-27 19:40:34 -0400

    Tests for new baseextendedidling tier (#351)

--- test/e2e/nstemplatetier_test.go
+++ test/e2e/nstemplatetier_test.go
@@ -37,7 +37,7 @@ func TestNSTemplateTiers(t *testing.T) {
 	testingtiers := CreateAndApproveSignup(t, hostAwait, testingTiersName, memberAwait.ClusterName)
 
 	// all tiers to check - keep the base as the last one, it will verify downgrade back to the default tier at the end of the test
-	tiersToCheck := []string{"advanced", "team", "test", "basedeactivationdisabled", "baseextended", "base"}
+	tiersToCheck := []string{"advanced", "team", "test", "basedeactivationdisabled", "baseextended", "baseextendedidling", "base"}
 
 	// when the tiers are created during the startup then we can verify them
 	allTiers := &toolchainv1alpha1.NSTemplateTierList{}
@@ -258,7 +258,7 @@ func TestTierTemplates(t *testing.T) {
 	// when the tiers are created during the startup then we can verify them
 	allTiers := &toolchainv1alpha1.TierTemplateList{}
 	err := hostAwait.Client.List(context.TODO(), allTiers, client.InNamespace(hostAwait.Namespace))
-	// verify that we have 18 tier templates (base: 3, baseextended: 3, basedeactivationdisabled 3, advanced: 3, team 3, test 3)
+	// verify that we have 21 tier templates (base: 3, baseextended: 3, baseextendedidling: 3, basedeactivationdisabled: 3, advanced: 3, team: 3, test: 3)
 	require.NoError(t, err)
-	assert.Len(t, allTiers.Items, 18)
+	assert.Len(t, allTiers.Items, 21)
 }
