5afd43c47dea1b65c0edda7b5f4f0cf7a52e2367
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-10-01 10:09:40 -0400

    Refactoring

--- setup/metrics/gather.go
+++ setup/metrics/gather.go
@@ -18,15 +18,19 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type Gatherer struct {
-	client           client.Client
-	interval         time.Duration
+var (
+	k8sClient        client.Client
+	queryInterval    time.Duration
 	prometheusClient prometheus.API
-	queries          []*queries.Query
+	mqueries         []*queries.Query
 	term             terminal.Terminal
-}
+)
 
-func New(term terminal.Terminal, cl client.Client, token string, interval time.Duration) Gatherer {
+func Init(t terminal.Terminal, cl client.Client, token string, interval time.Duration) {
+	k8sClient = cl
+	queryInterval = interval
+	term = t
+
 	url, err := getPrometheusEndpoint(cl)
 	if err != nil {
 		term.Fatalf(err, "error creating client: failed to get prometheus endpoint")
@@ -37,16 +41,9 @@ func New(term terminal.Terminal, cl client.Client, token string, interval time.D
 		term.Fatalf(err, "error creating client")
 	}
 
-	promClient := prometheus.NewAPI(httpClient)
+	prometheusClient = prometheus.NewAPI(httpClient)
 
-	g := Gatherer{
-		client:           cl,
-		interval:         interval,
-		prometheusClient: promClient,
-		term:             term,
-	}
-
-	g.queries = append(g.queries,
+	mqueries = append(mqueries,
 		queries.QueryClusterCPUUtilisation(),
 		queries.QueryClusterMemoryUtilisation(),
 		queries.QueryEtcdMemoryUsage(),
@@ -57,39 +54,37 @@ func New(term terminal.Terminal, cl client.Client, token string, interval time.D
 		queries.QueryWorkloadCPUUsage(cfg.MemberOperatorNamespace, cfg.MemberOperatorWorkload),
 		queries.QueryWorkloadMemoryUsage(cfg.MemberOperatorNamespace, cfg.MemberOperatorWorkload),
 	)
-
-	return g
 }
 
-func (g *Gatherer) AddQueries(queries ...*queries.Query) {
-	g.queries = append(g.queries, queries...)
+func AddQueries(queries ...*queries.Query) {
+	mqueries = append(mqueries, queries...)
 }
 
-func (g *Gatherer) Start() chan struct{} {
-	if len(g.queries) == 0 {
-		g.term.Infof("Metrics gatherer has no queries defined, skipping metrics gathering...")
+func StartGathering() chan struct{} {
+	if len(mqueries) == 0 {
+		term.Infof("Metrics gatherer has no queries defined, skipping metrics gathering...")
 		return nil
 	}
 
 	stop := make(chan struct{})
 	go func() {
 		k8sutil.Until(func() {
-			for _, q := range g.queries {
-				_, warnings, err := q.DoQuery(g.prometheusClient)
+			for _, q := range mqueries {
+				_, warnings, err := q.DoQuery(prometheusClient)
 				if err != nil {
 					if strings.Contains(err.Error(), "client error: 403") {
-						url, tokenErr := auth.GetTokenRequestURI(g.client)
+						url, tokenErr := auth.GetTokenRequestURI(k8sClient)
 						if tokenErr == nil {
-							g.term.Fatalf(err, "metrics query failed with 403 (Forbidden) - retrieve a new token from %s", url)
+							term.Fatalf(err, "metrics query failed with 403 (Forbidden) - retrieve a new token from %s", url)
 						}
 					}
-					g.term.Fatalf(err, "metrics query failed - check whether prometheus is still healthy in the cluster")
+					term.Fatalf(err, "metrics query failed - check whether prometheus is still healthy in the cluster")
 				} else if len(warnings) > 0 {
-					g.term.Fatalf(fmt.Errorf("%v", warnings), "metrics query had unexpected warnings")
+					term.Fatalf(fmt.Errorf("%v", warnings), "metrics query had unexpected warnings")
 				}
 
 			}
-		}, g.interval, stop)
+		}, queryInterval, stop)
 	}()
 	return stop
 }
@@ -105,8 +100,8 @@ func getPrometheusEndpoint(client client.Client) (string, error) {
 	return "https://" + prometheusRoute.Spec.Host, nil
 }
 
-func (g *Gatherer) PrintResults() {
-	for _, q := range g.queries {
-		g.term.Infof("%s: %s", q.Name, q.Result())
+func PrintResults() {
+	for _, q := range mqueries {
+		term.Infof("%s: %s", q.Name, q.Result())
 	}
 }
