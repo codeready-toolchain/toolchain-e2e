8af095366bbe6de69c27ccb5ab151d4aa67f5805
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-11-19 10:40:38 -0500

    test create a resource in the other users namespace

--- test/e2e/appstudio_test.go
+++ test/e2e/appstudio_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"testing"
-	"time"
 
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	. "github.com/codeready-toolchain/toolchain-e2e/testsupport"
@@ -46,141 +45,151 @@ func TestAppStudioFlow(t *testing.T) {
 	users := []appstudioUsers{
 		{
 			expectedMemberCluster: memberAwait,
-			username:              "asm1",
+			username:              "appstudiomember1",
 		},
 		{
 			expectedMemberCluster: memberAwait2,
-			username:              "asm2",
+			username:              "appstudiomember2",
 		},
 	}
 
-	for _, user := range users {
-		// Create and approve signup
-		req := NewSignupRequest(t, awaitilities).
-			Username(user.username).
-			ManuallyApprove().
-			TargetCluster(user.expectedMemberCluster).
-			EnsureMUR().
-			RequireConditions(ConditionSet(Default(), ApprovedByAdmin())...).
-			Execute()
+	for index, user := range users {
+		t.Run(user.username, func(t *testing.T) {
+			// Create and approve signup
+			req := NewSignupRequest(t, awaitilities).
+				Username(user.username).
+				ManuallyApprove().
+				TargetCluster(user.expectedMemberCluster).
+				EnsureMUR().
+				RequireConditions(ConditionSet(Default(), ApprovedByAdmin())...).
+				Execute()
 
-		user.signup, _ = req.Resources()
-		user.token = req.GetToken()
+			user.signup, _ = req.Resources()
+			user.token = req.GetToken()
 
-		VerifyResourcesProvisionedForSignup(t, awaitilities, user.signup, "base")
-		_, err = hostAwait.GetMasterUserRecord(wait.WithMurName(user.username))
-		require.NoError(t, err)
-
-		// since the registration service always provisions users to the default tier users need to be
-		// promoted to the appstudio tier in order to test appstudio scenarios
-		t.Run("promote to appstudio tier", func(t *testing.T) {
-			// given
-			promotionTier := "appstudio"
-
-			changeTierRequest := NewChangeTierRequest(hostAwait.Namespace, user.signup.Status.CompliantUsername, promotionTier)
-
-			// when
-			err = hostAwait.CreateWithCleanup(context.TODO(), changeTierRequest)
-
-			// then
+			VerifyResourcesProvisionedForSignup(t, awaitilities, user.signup, "base")
+			_, err = hostAwait.GetMasterUserRecord(wait.WithMurName(user.username))
 			require.NoError(t, err)
-			_, err := hostAwait.WaitForChangeTierRequest(changeTierRequest.Name, toBeComplete)
-			require.NoError(t, err)
-			VerifyResourcesProvisionedForSignup(t, awaitilities, user.signup, promotionTier)
 
-			// then - wait until ChangeTierRequest is deleted by our automatic GC
-			err = hostAwait.WaitUntilChangeTierRequestDeleted(changeTierRequest.Name)
-			assert.NoError(t, err)
-		})
+			// since the registration service always provisions users to the default tier users need to be
+			// promoted to the appstudio tier in order to test appstudio scenarios
+			t.Run("promote to appstudio tier", func(t *testing.T) {
+				// given
+				promotionTier := "appstudio"
 
-		t.Run("use proxy to create a configmap in each user appstudio namespace via proxy API", func(t *testing.T) {
-			cmName := "appstudio-test-cm"
-			expectedCM := &corev1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      cmName,
-					Namespace: user.username,
-				},
-				Data: map[string]string{
-					"planet": "venus",
-				},
+				changeTierRequest := NewChangeTierRequest(hostAwait.Namespace, user.signup.Status.CompliantUsername, promotionTier)
+
+				// when
+				err = hostAwait.CreateWithCleanup(context.TODO(), changeTierRequest)
+
+				// then
+				require.NoError(t, err)
+				_, err := hostAwait.WaitForChangeTierRequest(changeTierRequest.Name, toBeComplete)
+				require.NoError(t, err)
+				VerifyResourcesProvisionedForSignup(t, awaitilities, user.signup, promotionTier)
+
+				// then - wait until ChangeTierRequest is deleted by our automatic GC
+				err = hostAwait.WaitUntilChangeTierRequestDeleted(changeTierRequest.Name)
+				assert.NoError(t, err)
+			})
+
+			t.Run("use proxy to create a configmap in each user appstudio namespace via proxy API", func(t *testing.T) {
+				cmName := fmt.Sprintf("%s-test-cm", user.username)
+				expectedCM := &corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      cmName,
+						Namespace: user.username,
+					},
+					Data: map[string]string{
+						"planet": "venus",
+					},
+				}
+
+				proxyCl := hostAwait.CreateAPIProxyClient(user.token)
+				err := proxyCl.Create(context.TODO(), expectedCM)
+				require.NoError(t, err)
+
+				createdCM := &corev1.ConfigMap{}
+				err = proxyCl.Get(context.TODO(), types.NamespacedName{Namespace: user.username, Name: cmName}, createdCM)
+				require.NoError(t, err)
+
+				require.NoError(t, err)
+				require.NotEmpty(t, createdCM)
+				require.Equal(t, "venus", createdCM.Data["planet"])
+			})
+
+			t.Run("try to create a resource in an unauthorized namespace", func(t *testing.T) {
+				cmName := fmt.Sprintf("%s-appstudio-test-cm", user.username)
+				expectedCM := &corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      cmName,
+						Namespace: hostAwait.Namespace, // user should not be allowed to create a resource in the host operator namespace
+					},
+					Data: map[string]string{
+						"planet": "venus",
+					},
+				}
+
+				proxyCl := hostAwait.CreateAPIProxyClient(user.token)
+				err := proxyCl.Create(context.TODO(), expectedCM)
+				require.EqualError(t, err, fmt.Sprintf(`configmaps is forbidden: User "system:serviceaccount:%[1]s:appstudio-%[1]s" cannot create resource "configmaps" in API group "" in the namespace "%[2]s"`, user.username, hostAwait.Namespace))
+			})
+
+			if index == 1 { // only for the second user
+				t.Run("try to create a resource in the other users namespace", func(t *testing.T) {
+					cmName := fmt.Sprintf("%s-appstudio-test-cm", users[0].username)
+					expectedCM := &corev1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      cmName,
+							Namespace: users[0].expectedMemberCluster.Namespace, // user should not be allowed to create a resource in the first user's namespace
+						},
+						Data: map[string]string{
+							"planet": "venus",
+						},
+					}
+
+					proxyCl := hostAwait.CreateAPIProxyClient(user.token)
+					err := proxyCl.Create(context.TODO(), expectedCM)
+					require.EqualError(t, err, fmt.Sprintf(`configmaps is forbidden: User "system:serviceaccount:%[1]s:appstudio-%[1]s" cannot create resource "configmaps" in API group "" in the namespace "%[2]s"`, user.username, users[0].expectedMemberCluster.Namespace))
+				})
 			}
 
-			proxyCl := hostAwait.CreateAPIProxyClient(user.token)
-			err := proxyCl.Create(context.TODO(), expectedCM)
-			require.NoError(t, err)
+			t.Run("delete usersignup and expect all resources to be deleted", func(t *testing.T) {
+				// given
+				signup, err := hostAwait.WaitForUserSignup(user.signup.Name)
+				require.NoError(t, err)
 
-			createdCM := &corev1.ConfigMap{}
-			err = proxyCl.Get(context.TODO(), types.NamespacedName{Namespace: user.username, Name: cmName}, createdCM)
-			require.NoError(t, err)
+				// when
+				err = hostAwait.Client.Delete(context.TODO(), user.signup)
 
-			require.NoError(t, err)
-			require.NotEmpty(t, createdCM)
-			require.Equal(t, "venus", createdCM.Data["planet"])
-		})
+				// then
+				require.NoError(t, err)
+				t.Logf("usersignup '%s' deleted (resource name='%s')", user.username, signup.Name)
 
-		t.Run("try to create a resource in an unauthorized namespace", func(t *testing.T) {
-			cmName := fmt.Sprintf("%s-appstudio-test-cm", user.username)
-			expectedCM := &corev1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      cmName,
-					Namespace: hostAwait.Namespace, // user should not be allowed to create a resource in the host operator namespace
-				},
-				Data: map[string]string{
-					"planet": "venus",
-				},
-			}
+				err = hostAwait.WaitUntilMasterUserRecordDeleted(user.username)
+				assert.NoError(t, err, "MasterUserRecord is not deleted")
 
-			proxyCl := hostAwait.CreateAPIProxyClient(user.token)
-			err := proxyCl.Create(context.TODO(), expectedCM)
-			require.NoError(t, err)
+				err = memberAwait.WaitUntilUserAccountDeleted(user.username)
+				assert.NoError(t, err, "UserAccount is not deleted")
 
-			t.Log("sleeping for 1 minute...")
-			time.Sleep(1 * time.Minute)
+				err = memberAwait.WaitUntilUserDeleted(user.username)
+				assert.NoError(t, err, "User is not deleted")
 
-			createdCM := &corev1.ConfigMap{}
-			err = proxyCl.Get(context.TODO(), types.NamespacedName{Namespace: user.username, Name: cmName}, createdCM)
-			require.NoError(t, err)
+				err = memberAwait.WaitUntilIdentityDeleted(user.username)
+				assert.NoError(t, err, "Identity is not deleted")
 
-			require.NoError(t, err)
-			require.NotEmpty(t, createdCM)
-			require.Equal(t, "venus", createdCM.Data["planet"])
-		})
+				err = memberAwait.WaitUntilNSTemplateSetDeleted(user.username)
+				assert.NoError(t, err, "NSTemplateSet id not deleted")
 
-		t.Run("delete usersignup and expect all resources to be deleted", func(t *testing.T) {
-			// given
-			signup, err := hostAwait.WaitForUserSignup(user.signup.Name)
-			require.NoError(t, err)
+				err = memberAwait.WaitUntilClusterResourceQuotasDeleted(user.username)
+				assert.NoError(t, err, "ClusterResourceQuotas were not deleted")
 
-			// when
-			err = hostAwait.Client.Delete(context.TODO(), user.signup)
+				err = memberAwait.WaitUntilNamespaceDeleted(user.username, "dev")
+				assert.NoError(t, err, "johnsmith-dev namespace is not deleted")
 
-			// then
-			require.NoError(t, err)
-			t.Logf("usersignup '%s' deleted (resource name='%s')", user.username, signup.Name)
-
-			err = hostAwait.WaitUntilMasterUserRecordDeleted(user.username)
-			assert.NoError(t, err, "MasterUserRecord is not deleted")
-
-			err = memberAwait.WaitUntilUserAccountDeleted(user.username)
-			assert.NoError(t, err, "UserAccount is not deleted")
-
-			err = memberAwait.WaitUntilUserDeleted(user.username)
-			assert.NoError(t, err, "User is not deleted")
-
-			err = memberAwait.WaitUntilIdentityDeleted(user.username)
-			assert.NoError(t, err, "Identity is not deleted")
-
-			err = memberAwait.WaitUntilNSTemplateSetDeleted(user.username)
-			assert.NoError(t, err, "NSTemplateSet id not deleted")
-
-			err = memberAwait.WaitUntilClusterResourceQuotasDeleted(user.username)
-			assert.NoError(t, err, "ClusterResourceQuotas were not deleted")
-
-			err = memberAwait.WaitUntilNamespaceDeleted(user.username, "dev")
-			assert.NoError(t, err, "johnsmith-dev namespace is not deleted")
-
-			err = memberAwait.WaitUntilNamespaceDeleted(user.username, "stage")
-			assert.NoError(t, err, "johnsmith-stage namespace is not deleted")
+				err = memberAwait.WaitUntilNamespaceDeleted(user.username, "stage")
+				assert.NoError(t, err, "johnsmith-stage namespace is not deleted")
+			})
 		})
 	} // end users loop
 
