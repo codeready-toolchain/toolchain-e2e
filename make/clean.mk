af1d2f022174bc685c16ff38ce985a5eb7b26eb2
Matous Jobanek <mjobanek@redhat.com>
2021-12-20 11:14:49 +0100

    feat: add appstudio-e2e-deploy-latest target

--- make/clean.mk
+++ make/clean.mk
@@ -11,18 +11,40 @@ clean:
 ##    * all usersignups including user namespaces
 clean-users:
 	$(Q)-oc delete usersignups --all --all-namespaces
+	$(Q)-oc delete spaces --all --all-namespaces
 	$(Q)-oc wait --for=delete namespaces -l toolchain.dev.openshift.com/provider=codeready-toolchain
 
-.PHONY: clean-e2e-resources
-## Delete resources in the OpenShift cluster. The deleted resources are:
-##    * all resources created by both host and member operators in all namespaces including user namespaces
-##    * operator namespaces created during both the dev and e2e test setup (for both operators host and member)
-##    * all CatalogSources that were created as part of operator deployment
-clean-e2e-resources: clean-users
-	$(Q)-oc get projects --output=name | grep -E "toolchain-(member|host)(\-operator)?(\-[0-9]+)?" | xargs oc delete
+.PHONY: clean-cluster-wide-config
+## Delete all cluster-wide configuration resources like PriorityClass, MutatingWebhookConfiguration, and ClusterRoleBinding for e2e SA
+clean-cluster-wide-config:
 	$(Q)-oc get ClusterRoleBinding -o name | grep e2e-service-account | xargs oc delete
 	$(Q)-oc delete PriorityClass -l='toolchain.dev.openshift.com/provider=codeready-toolchain'
 	$(Q)-oc delete MutatingWebhookConfiguration -l='toolchain.dev.openshift.com/provider=codeready-toolchain'
+
+.PHONY: clean-toolchain-namespaces-in-e2e
+## Delete e2e namespaces
+clean-toolchain-namespaces-in-e2e:
+	$(Q)-oc get projects --output=name | grep -E "toolchain-(member|host)(\-operator)?(\-[0-9]+)?" | xargs oc delete
+
+.PHONY: clean-e2e-resources
+## Delete resources in the OpenShift cluster. The deleted resources are:
+##    * all user-related resources
+##    * operator namespaces created during both the dev and e2e test setup (for both operators host and member)
+##    * cluster-wide config
+clean-e2e-resources: clean-users clean-toolchain-namespaces-in-e2e clean-cluster-wide-config
+
+.PHONY: clean-toolchain-namespaces-in-dev
+## Delete dev namespaces
+clean-toolchain-namespaces-in-dev:
+	$(Q)oc delete namespace ${DEV_HOST_NS} || true
+	$(Q)oc delete namespace ${DEV_MEMBER_NS} || true
+
+.PHONY: clean-dev-resources
+## Delete resources in the OpenShift cluster. The deleted resources are:
+##    * all user-related resources
+##    * operator namespaces created during both the dev and e2e test setup (for both operators host and member)
+##    * cluster-wide config
+clean-dev-resources: clean-users clean-toolchain-namespaces-in-dev clean-cluster-wide-config
 
 .PHONY: clean-e2e-files
 ## Remove files and directories used during e2e test setup
@@ -30,29 +52,19 @@ clean-e2e-files:
 	rm -f ${WAS_ALREADY_PAIRED_FILE} 2>/dev/null || true
 	rm -rf ${IMAGE_NAMES_DIR} 2>/dev/null || true
 
-.PHONY: clean-toolchain-resources-in-dev
+.PHONY: clean-all-toolchain-resources
 ## Delete resources in the OpenShift cluster. The deleted resources are:
 ##    * all toolchain.dev.openshift.com CRs in both host and member namespaces created during the dev setup
 ##    * all ClusterresourceQuotas with the label toolchain.dev.openshift.com/provider=codeready-toolchain in member namespace
-##    * pods of both operators host and member
-clean-toolchain-resources-in-dev:
-	$(Q)echo "cleaning resources in $(DEV_HOST_NS) and $(DEV_MEMBER_NS)..."
-	$(Q)oc get usersignups -n $(DEV_HOST_NS) -o name | xargs -i oc delete -n $(DEV_HOST_NS) {}
+clean-all-toolchain-resources:
+	$(Q)echo "cleaning resources"
+	$(Q)oc delete usersignups --all --all-namespaces
 	$(Q)for CRD in `oc get crd -o name | grep toolchain`; do \
-		echo "deleting $${CRD} in $(DEV_HOST_NS) and $(DEV_MEMBER_NS)"; \
+		echo "deleting $${CRD}"; \
 		CRD_NAME=`oc get $${CRD} --template '{{.metadata.name}}'`; \
-		oc get $${CRD_NAME} -n $(DEV_HOST_NS) -o name | xargs -i oc delete {}; \
-		oc get $${CRD_NAME} -n $(DEV_MEMBER_NS) -o name | xargs -i oc delete {}; \
+		oc delete $${CRD_NAME} --all --all-namespaces; \
 	done
-	$(Q)oc get clusterresourcequotas -l "toolchain.dev.openshift.com/provider"=codeready-toolchain -o name -n $(DEV_MEMBER_NS) | xargs -i oc delete {}
-	$(Q)oc get pods -n $(DEV_HOST_NS) -l name=host-operator -o name | xargs -i oc delete -n $(DEV_HOST_NS) {}
-	$(Q)oc get pods -n $(DEV_MEMBER_NS) -l name=member-operator -o name | xargs -i oc delete -n $(DEV_MEMBER_NS) {}
-
-.PHONY: clean-toolchain-namespaces-in-dev
-## Delete dev namespaces
-clean-toolchain-namespaces-in-dev:
-	$(Q)oc delete namespace ${DEV_HOST_NS} || true
-	$(Q)oc delete namespace ${DEV_MEMBER_NS} || true
+	$(Q)oc get clusterresourcequotas -l "toolchain.dev.openshift.com/provider"=codeready-toolchain --all --all-namespaces
 
 .PHONY: clean-toolchain-crds
 ## Delete all Toolchain CRDs
