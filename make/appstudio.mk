254020f4be4416069994f2fc06991518dbae28c1
Matous Jobanek <mjobanek@redhat.com>
2021-12-22 23:20:57 +0100

    feat: add appstudio-e2e-deploy-latest target (#427)
    * feat: add appstudio-e2e-deploy-latest target
    
    * fix
    
    Co-authored-by: Alexey Kazakov <alkazako@redhat.com>

---
+++ make/appstudio.mk
@@ -0,0 +1,10 @@
+.PHONY: appstudio-dev-deploy-latest
+appstudio-dev-deploy-latest: DEV_ENVIRONMENT=appstudio-dev
+appstudio-dev-deploy-latest: dev-deploy-latest
+
+.PHONY: appstudio-e2e-deploy-latest
+appstudio-e2e-deploy-latest: DEV_ENVIRONMENT=appstudio-e2e
+appstudio-e2e-deploy-latest: dev-deploy-latest
+
+.PHONY: appstudio-cleanup
+appstudio-cleanup: clean-dev-resources
