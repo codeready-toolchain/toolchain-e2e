a05a45193e69efacf8a52b7dcb1276eafdf3c497
Matous Jobanek <mjobanek@redhat.com>
2022-01-18 08:30:09 +0100

    use master

--- make/run-cicd-script.mk
+++ make/run-cicd-script.mk
@@ -1,5 +1,5 @@
 
-OWNER_AND_BRANCH_LOCATION=MatousJobanek/toolchain-cicd/support-deploy-latest
+OWNER_AND_BRANCH_LOCATION=codeready-toolchain/toolchain-cicd/master
 GH_SCRIPTS_URL=https://raw.githubusercontent.com/${OWNER_AND_BRANCH_LOCATION}
 
 .PHONY: run-cicd-script
