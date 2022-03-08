c288b650eb6f8df5cbb6800f59bbc0b1ebae3856
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-03-08 08:27:43 -0500

    test migration from MUR to Spaces (#475)

--- testsupport/spacebinding.go
+++ testsupport/spacebinding.go
@@ -5,14 +5,28 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/codeready-toolchain/api/api/v1alpha1"
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/toolchain-e2e/testsupport/wait"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// VerifySpaceBinding waits until a spacebinding with the given mur and space name exists and then verifies the contents are correct
+func VerifySpaceBinding(t *testing.T, hostAwait *wait.HostAwaitility, murName, spaceName, spaceRole string) *toolchainv1alpha1.SpaceBinding {
+
+	spaceBinding, err := hostAwait.WaitForSpaceBinding(murName, spaceName,
+		wait.UntilSpaceBindingHasMurName(murName),
+		wait.UntilSpaceBindingHasSpaceName(spaceName),
+		wait.UntilSpaceBindingHasSpaceRole(spaceRole),
+	)
+
+	require.NoError(t, err)
+
+	return spaceBinding
+}
+
 // CreateSpaceBinding creates SpaceBinding resource for the given MUR & Space with the given space role
-func CreateSpaceBinding(t *testing.T, hostAwait *wait.HostAwaitility, mur *v1alpha1.MasterUserRecord, space *v1alpha1.Space, spaceRole string) *v1alpha1.SpaceBinding {
+func CreateSpaceBinding(t *testing.T, hostAwait *wait.HostAwaitility, mur *toolchainv1alpha1.MasterUserRecord, space *toolchainv1alpha1.Space, spaceRole string) *toolchainv1alpha1.SpaceBinding {
 	spaceBinding := NewSpaceBinding(hostAwait.Namespace, mur.Name, space.Name, spaceRole)
 	err := hostAwait.CreateWithCleanup(context.TODO(), spaceBinding)
 	require.NoError(t, err)
@@ -21,7 +35,7 @@ func CreateSpaceBinding(t *testing.T, hostAwait *wait.HostAwaitility, mur *v1alp
 }
 
 // CreateSpaceBindingWithoutCleanup creates SpaceBinding resource for the given MUR & Space with the given space role; and doesn't mark the resource to be ready for cleanup
-func CreateSpaceBindingWithoutCleanup(t *testing.T, hostAwait *wait.HostAwaitility, mur *v1alpha1.MasterUserRecord, space *v1alpha1.Space, spaceRole string) *v1alpha1.SpaceBinding {
+func CreateSpaceBindingWithoutCleanup(t *testing.T, hostAwait *wait.HostAwaitility, mur *toolchainv1alpha1.MasterUserRecord, space *toolchainv1alpha1.Space, spaceRole string) *toolchainv1alpha1.SpaceBinding {
 	spaceBinding := NewSpaceBinding(hostAwait.Namespace, mur.Name, space.Name, spaceRole)
 	err := hostAwait.Client.Create(context.TODO(), spaceBinding)
 	require.NoError(t, err)
@@ -30,17 +44,17 @@ func CreateSpaceBindingWithoutCleanup(t *testing.T, hostAwait *wait.HostAwaitili
 }
 
 // NewSpaceBinding create an object SpaceBinding with the given values
-func NewSpaceBinding(namespace, mur, space, spaceRole string) *v1alpha1.SpaceBinding {
-	return &v1alpha1.SpaceBinding{
+func NewSpaceBinding(namespace, mur, space, spaceRole string) *toolchainv1alpha1.SpaceBinding {
+	return &toolchainv1alpha1.SpaceBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("%s-%s", mur, space),
 			Namespace: namespace,
 			Labels: map[string]string{
-				v1alpha1.SpaceBindingMasterUserRecordLabelKey: mur,
-				v1alpha1.SpaceBindingSpaceLabelKey:            space,
+				toolchainv1alpha1.SpaceBindingMasterUserRecordLabelKey: mur,
+				toolchainv1alpha1.SpaceBindingSpaceLabelKey:            space,
 			},
 		},
-		Spec: v1alpha1.SpaceBindingSpec{
+		Spec: toolchainv1alpha1.SpaceBindingSpec{
 			MasterUserRecord: mur,
 			Space:            space,
 			SpaceRole:        spaceRole,
