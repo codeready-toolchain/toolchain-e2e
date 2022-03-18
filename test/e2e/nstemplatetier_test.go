9adddd0dc8b390954bbca30669ebc3ffbf0c82d5
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-03-18 18:39:39 -0400

    Tests for new single namespace tier (#499)
    * test updates for single namespace tier

--- test/e2e/nstemplatetier_test.go
+++ test/e2e/nstemplatetier_test.go
@@ -50,7 +50,7 @@ func TestNSTemplateTiers(t *testing.T) {
 		Resources()
 
 	// all tiers to check - keep the base as the last one, it will verify downgrade back to the default tier at the end of the test
-	tiersToCheck := []string{"advanced", "basedeactivationdisabled", "baseextended", "baseextendedidling", "baselarge", "hackathon", "test", "appstudio", "base"}
+	tiersToCheck := []string{"advanced", "basedeactivationdisabled", "baseextended", "baseextendedidling", "baselarge", "hackathon", "test", "appstudio", "base1ns", "base"}
 
 	// when the tiers are created during the startup then we can verify them
 	allTiers := &toolchainv1alpha1.NSTemplateTierList{}
@@ -404,8 +404,8 @@ func TestTierTemplates(t *testing.T) {
 	// when the tiers are created during the startup then we can verify them
 	allTiers := &toolchainv1alpha1.TierTemplateList{}
 	err = hostAwait.Client.List(context.TODO(), allTiers, client.InNamespace(hostAwait.Namespace), notCreatedByE2e)
-	// verify that we have 26 tier templates (base: 3, baselarge: 3, baseextended: 3, baseextendedidling: 3, basedeactivationdisabled: 3, advanced: 3, test: 3, hackathon: 3, appstudio: 2)
+	// verify that we have 28 tier templates (base: 3, base1ns: 2, baselarge: 3, baseextended: 3, baseextendedidling: 3, basedeactivationdisabled: 3, advanced: 3, test: 3, hackathon: 3, appstudio: 2)
 	require.NoError(t, err)
 	// we cannot verify the exact number of tiers, because during the operator update it may happen that more TierTemplates are created
-	assert.True(t, len(allTiers.Items) >= 26)
+	assert.True(t, len(allTiers.Items) >= 28)
 }
