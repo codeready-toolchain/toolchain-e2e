67be9ec0d377a767dd864ad0521acf2b23d9ea93
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-03-22 18:43:31 -0400

    Test save logs to artifacts dir

--- make/run-cicd-script.mk
+++ make/run-cicd-script.mk
@@ -1,5 +1,5 @@
 
-OWNER_AND_BRANCH_LOCATION=codeready-toolchain/toolchain-cicd/master
+OWNER_AND_BRANCH_LOCATION=rajivnathan/toolchain-cicd/saveLogsToArtifactsDir
 GH_SCRIPTS_URL=https://raw.githubusercontent.com/${OWNER_AND_BRANCH_LOCATION}
 USE_LOCAL_SCRIPTS=false
 
