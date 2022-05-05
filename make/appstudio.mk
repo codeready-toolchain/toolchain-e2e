6fa024326bc8c7bc092ad57517a373cf80d74f1e
Matous Jobanek <mjobanek@redhat.com>
2022-05-05 08:31:11 +0200

    fix(appstudio): use latest tag in CI (#549)

--- make/appstudio.mk
+++ make/appstudio.mk
@@ -1,9 +1,11 @@
 .PHONY: appstudio-dev-deploy-latest
 appstudio-dev-deploy-latest: DEV_ENVIRONMENT=appstudio-dev
+appstudio-dev-deploy-latest: DEPLOY_LATEST=true
 appstudio-dev-deploy-latest: dev-deploy-e2e
 
 .PHONY: appstudio-e2e-deploy-latest
 appstudio-e2e-deploy-latest: DEV_ENVIRONMENT=appstudio-e2e
+appstudio-e2e-deploy-latest: DEPLOY_LATEST=true
 appstudio-e2e-deploy-latest: dev-deploy-e2e
 
 .PHONY: appstudio-cleanup
