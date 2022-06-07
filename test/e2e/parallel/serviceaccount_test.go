b7b2d1523e0753a002335adff80c80438b7214fd
Matous Jobanek <mjobanek@redhat.com>
2022-06-07 12:51:48 +0200

    fix

--- test/e2e/parallel/serviceaccount_test.go
+++ test/e2e/parallel/serviceaccount_test.go
@@ -3,11 +3,13 @@ package parallel
 import (
 	"context"
 	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/codeready-toolchain/api/api/v1alpha1"
 	. "github.com/codeready-toolchain/toolchain-e2e/testsupport"
 	"github.com/codeready-toolchain/toolchain-e2e/testsupport/tiers"
+	"github.com/codeready-toolchain/toolchain-e2e/testsupport/wait"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
@@ -37,6 +39,7 @@ func TestDoNotOverrideServiceAccount(t *testing.T) {
 	// get the SA that is provisioned for the user in the ns
 	sa, err := member.WaitForServiceAccount(mur.Name, fmt.Sprintf("appstudio-%s", mur.Name))
 	require.NoError(t, err)
+	expectedSecrets := getSASecrets(t, member, mur.Name, sa.Name)
 
 	// when we add an annotation to the SA resource then it should stay there
 	sa.Annotations = map[string]string{
@@ -57,22 +60,21 @@ func TestDoNotOverrideServiceAccount(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, "stay", sa.Annotations["should"])
 
-	// verify the secrets created for SA
-	// list all secrets in the namespace
+	// verify that the secrets created for SA is the same
+	assert.Equal(t, expectedSecrets, getSASecrets(t, member, mur.Name, sa.Name))
+
+}
+
+func getSASecrets(t *testing.T, member *wait.MemberAwaitility, ns, saName string) []string {
+	var saSecrets []string
 	secrets := &corev1.SecretList{}
-	require.NoError(t, member.Client.List(context.TODO(), secrets, client.InNamespace(mur.Name)))
+	require.NoError(t, member.Client.List(context.TODO(), secrets, client.InNamespace(ns)))
 	for _, secret := range secrets.Items {
 		// filter only those that are marked to be owned by the SA
-		if secret.Annotations[corev1.ServiceAccountNameKey] == sa.Name {
-			found := false
-			// check if the secret is listed in the SA's secrets
-			for _, saSecret := range sa.Secrets {
-				if saSecret.Name == secret.Name {
-					found = true
-					break
-				}
-			}
-			assert.True(t, found, "the secret %s is marked to be owned by SA % but it wasn't listed in the secrets of the SA: %v", secret.Name, sa.Namespace, sa.Secrets)
+		if secret.Annotations[corev1.ServiceAccountNameKey] == saName {
+			saSecrets = append(saSecrets, secret.Name)
 		}
 	}
+	sort.Strings(saSecrets)
+	return saSecrets
 }
