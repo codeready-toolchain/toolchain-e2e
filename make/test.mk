eb496406e91730e10adf2e950e0d54ac54374ba6
Francisc Munteanu <fmuntean@redhat.com>
2023-03-08 11:53:33 +0100

    handle case in which toolchaincluster name is longer then 63 chars

--- make/test.mk
+++ make/test.mk
@@ -330,16 +330,19 @@ create-host-project:
 .PHONY: create-host-resources
 create-host-resources:
 	# ignore if these resources already exist (nstemplatetiers may have already been created by operator)
-	-oc create -f deploy/host-operator/${ENVIRONMENT}/ -n ${HOST_NS}
+	- oc create -f deploy/host-operator/${ENVIRONMENT}/ -n ${HOST_NS}
 	# patch toolchainconfig to prevent webhook deploy for 2nd member, a 2nd webhook deploy causes the webhook verification in e2e tests to fail
 	# since e2e environment has 2 member operators running in the same cluster
+	# for details on how the TOOLCHAINCLUSTER_NAME is composed see https://github.com/codeready-toolchain/toolchain-cicd/blob/master/scripts/add-cluster.sh
 	if [[ ${SECOND_MEMBER_MODE} == true ]]; then \
 		API_ENDPOINT=`oc get infrastructure cluster -o jsonpath='{.status.apiServerURL}'`; \
-		TOOLCHAIN_CLUSTER_NAME=`echo "$${API_ENDPOINT}" | sed 's/.*api\.\([^:]*\):.*/\1/'`; \
 		echo "API_ENDPOINT $${API_ENDPOINT}"; \
+    	JOINING_CLUSTER_NAME=`echo "$${API_ENDPOINT}" | sed 's/^[^/]*\/\/\([^:/]*\)\(:.*\)\{0,1\}\(\/.*\)\{0,1\}$$/\1/' | sed 's/^api\.//'`; \
+    	echo "Joining cluster name: $${JOINING_CLUSTER_NAME}"; \
+		TOOLCHAIN_CLUSTER_NAME=`echo "member-$${JOINING_CLUSTER_NAME:0:55}2"`; \
 		echo "TOOLCHAIN_CLUSTER_NAME $${TOOLCHAIN_CLUSTER_NAME}"; \
 		PATCH_FILE=/tmp/patch-toolchainconfig_${DATE_SUFFIX}.json; \
-		echo "{\"spec\":{\"members\":{\"specificPerMemberCluster\":{\"member-$${TOOLCHAIN_CLUSTER_NAME}2\":{\"webhook\":{\"deploy\":false}}}}}}" > $$PATCH_FILE; \
+		echo "{\"spec\":{\"members\":{\"specificPerMemberCluster\":{\"$${TOOLCHAIN_CLUSTER_NAME}\":{\"webhook\":{\"deploy\":false}}}}}}" > $$PATCH_FILE; \
 		oc patch toolchainconfig config -n $(HOST_NS) --type=merge --patch "$$(cat $$PATCH_FILE)"; \
 	fi;
 ifneq ($(E2E_TEST_EXECUTION),true)
