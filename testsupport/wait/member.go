78374380acbdcd509ebe5a7caa53702fd9ddfb9f
Francisc Munteanu <fmuntean@redhat.com>
2023-03-10 07:59:22 +0100

    small fix in member status test

--- testsupport/wait/member.go
+++ testsupport/wait/member.go
@@ -1562,14 +1562,14 @@ func UntilMemberStatusHasUsageSet() MemberStatusWaitCriterion {
 			return hasMemberStatusUsageSet(actual.Status)
 		},
 		Diff: func(actual *toolchainv1alpha1.MemberStatus) string {
-			return fmt.Sprintf("expected MemberStatus to have 'master' and 'worker' usages set: %v", actual.Status.ResourceUsage.MemoryUsagePerNodeRole)
+			return fmt.Sprintf("expected MemberStatus to have 'master' and/or 'worker' usages set: %v", actual.Status.ResourceUsage.MemoryUsagePerNodeRole)
 		},
 	}
 }
 
 func hasMemberStatusUsageSet(status toolchainv1alpha1.MemberStatusStatus) bool {
 	usage := status.ResourceUsage.MemoryUsagePerNodeRole
-	return len(usage) == 1 && usage["worker"] > 0
+	return len(usage) >= 1 && usage["worker"] > 0
 }
 
 // UntilMemberStatusHasConsoleURLSet returns a `MemberStatusWaitCriterion` which checks that the given
