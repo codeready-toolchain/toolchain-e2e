d8055e53858dfb5693e551ad60321a12eea3981f
Francisc Munteanu <fmuntean@redhat.com>
2023-03-22 11:14:35 +0100

    set back cicd master branch

--- make/run-cicd-script.mk
+++ make/run-cicd-script.mk
@@ -1,5 +1,5 @@
 
-OWNER_AND_BRANCH_LOCATION=mfrancisc/toolchain-cicd/feature/ASC-299_hypershift_cluster
+OWNER_AND_BRANCH_LOCATION=codeready-toolchain/toolchain-cicd/master
 GH_SCRIPTS_URL=https://raw.githubusercontent.com/${OWNER_AND_BRANCH_LOCATION}
 USE_LOCAL_SCRIPTS=false
 
