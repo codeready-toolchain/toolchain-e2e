1e4c6fee0ef5d2ea2fa4b40a2e54427a5c7489bb
Francisc Munteanu <fmuntean@redhat.com>
2023-03-23 10:55:43 +0100

    revert emtpy line (no e2e test is triggered)

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -559,7 +559,6 @@ func TestE2EFlow(t *testing.T) {
 }
 
 func listByOwnerLabel(owner string) client.ListOption {
-
 	labels := map[string]string{toolchainv1alpha1.OwnerLabelKey: owner}
 	return client.MatchingLabels(labels)
 }
