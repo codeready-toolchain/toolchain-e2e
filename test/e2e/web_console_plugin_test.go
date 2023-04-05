b5f3be1249b85a39d7c6ce83f22f0cf604e34892
Shane Bryzak <sbryzak@gmail.com>
2023-04-05 20:42:36 +1000

    try with a route

--- test/e2e/web_console_plugin_test.go
+++ test/e2e/web_console_plugin_test.go
@@ -7,9 +7,12 @@ import (
 	. "github.com/codeready-toolchain/toolchain-e2e/testsupport"
 	"github.com/codeready-toolchain/toolchain-e2e/testsupport/wait"
 	"github.com/coreos/etcd/pkg/testutil"
+	v1 "github.com/openshift/api/route/v1"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"net/http"
 	"strings"
 	"testing"
@@ -51,13 +54,59 @@ func (s *webConsolePluginTest) TestWebConsoleDeployedSuccessfully() {
 		Execute(s.T())
 	response := signupRequest.GetSignupResponse(s.T())
 
+	_, mur := signupRequest.Resources()
+
+	memberAwait := GetMurTargetMember(s.T(), s.Awaitilities, mur)
+
 	// Response should contain a ConsoleURL with a value something like:
 	// https://console-openshift-console.apps.99b682869228f7464338-mgmt.ci.hypershift.devcluster.openshift.com/
 	require.Contains(s.T(), response, "consoleURL")
 
-	consoleURL := response["consoleURL"]
-	manifestURL := fmt.Sprintf("%s%s", consoleURL, "api/plugins/toolchain-member-web-console-plugin/plugin-manifest.json")
-	healthCheckURL := fmt.Sprintf("%s%s", consoleURL, "api/plugins/toolchain-member-web-console-plugin/status")
+	// Since we can't easily access the web console API resources directly (due to complex security requirements) we
+	// will instead create a route in the member cluster with which to access the console plugin
+
+	// Before we can create the route however, we need the certificate and private key values from the
+	// member-operator-console-plugin secret
+	secret, err := memberAwait.WaitForSecret(s.T(), memberAwait.Namespace, "member-operator-console-plugin")
+	require.NoError(s.T(), err)
+
+	// Now we can create the route resource
+	route := &v1.Route{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "consolepluginroute",
+			Namespace: memberAwait.Namespace,
+			Annotations: map[string]string{
+				"openshift.io/host.generated": "true",
+			},
+		},
+		Spec: v1.RouteSpec{
+			To: v1.RouteTargetReference{
+				Kind: "Service",
+				Name: "member-operator-console-plugin",
+			},
+			Port: &v1.RoutePort{
+				TargetPort: intstr.FromInt(9443),
+			},
+			TLS: &v1.TLSConfig{
+				Termination: "reencrypt",
+				Certificate: string(secret.Data["tls.crt"]),
+				Key:         string(secret.Data["tls.key"]),
+			},
+			WildcardPolicy: "None",
+		},
+	}
+
+	err = memberAwait.Create(s.T(), route)
+	require.NoError(s.T(), err)
+
+	consoleURL := response["consoleURL"].(string)
+
+	// From the Console URL we should be able to construct the Route URL
+	routeURL := strings.Replace(consoleURL, "console-openshift-console", "consolepluginroute-toolchain-member-operator", 1)
+
+	manifestURL := fmt.Sprintf("%s%s", routeURL, "plugin-manifest.json")
+	healthCheckURL := fmt.Sprintf("%s%s", routeURL, "status")
 
 	var tr *http.Transport
 	{ // #nosec G402
@@ -71,7 +120,6 @@ func (s *webConsolePluginTest) TestWebConsoleDeployedSuccessfully() {
 	httpClient := &http.Client{Transport: tr}
 
 	// First perform a health check - we will attempt up to 5 times to invoke the health check endpoint without error
-	var err error
 	var resp *http.Response
 
 	req, err := http.NewRequest("GET", healthCheckURL, nil)
