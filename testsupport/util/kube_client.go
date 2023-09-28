96535fbc385c6b13fbc84d0edb58a5c90434d074
Francesco Ilario <filario@redhat.com>
2023-09-28 16:24:10 +0200

    Add feature flag to disable TLS
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- testsupport/util/kube_client.go
+++ testsupport/util/kube_client.go
@@ -1,16 +1,22 @@
 package util
 
 import (
+	"os"
+
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 )
 
+const EnvDisableKubeClientTLSVerify string = "DISABLE_KUBE_CLIENT_TLS_VERIFY"
+
 func BuildKubernetesClient(apiConfig api.Config) (*rest.Config, error) {
-	insecureAPIConfig := setInsecureSkipTLSVerify(apiConfig)
+	if os.Getenv(EnvDisableKubeClientTLSVerify) == "true" {
+		apiConfig = setInsecureSkipTLSVerify(apiConfig)
+	}
 
 	configOverrides := clientcmd.ConfigOverrides{}
-	return clientcmd.NewDefaultClientConfig(insecureAPIConfig, &configOverrides).ClientConfig()
+	return clientcmd.NewDefaultClientConfig(apiConfig, &configOverrides).ClientConfig()
 }
 
 func setInsecureSkipTLSVerify(apiConfig api.Config) api.Config {
