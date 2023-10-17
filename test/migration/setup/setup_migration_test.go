8eeaeaf8f0c07e64a65df8dbba596fecfb28dd1f
Rajiv Senthilnathan <rajivnathan@gmail.com>
2023-10-16 21:03:38 -0400

    Add tests for VM webhook (#809)
    * Add tests for VM webhook
    
    * Update failure policies
    
    * Add comment to explain temporary disablement of webhook checks
    
    * Clarify comment
    
    * Add vm webhook test
    
    * Switch to unstructured type
    
    * Cleanup
    
    * Temporarily skip validating webhook migration test too
    
    * Fix lint errors
    
    * Fix lint error
    
    ---------
    
    Co-authored-by: Francisc Munteanu <fmuntean@redhat.com>
    Co-authored-by: Alexey Kazakov <alkazako@redhat.com>
    Co-authored-by: Matous Jobanek <mjobanek@redhat.com>

--- test/migration/setup/setup_migration_test.go
+++ test/migration/setup/setup_migration_test.go
@@ -9,6 +9,10 @@ import (
 
 func TestSetupMigration(t *testing.T) {
 	// given
+	// set env var to skip the mutating webhook check on migration setup temporarily since the old deployment
+	// will deploy the webhooks with the old configuration but the tests will be expecting the new configuration
+	// This should be removed after PR https://github.com/codeready-toolchain/toolchain-e2e/pull/809 is merged
+	t.Setenv("skip-webhook-checks-on-setup", "true")
 	awaitilities := WaitForDeployments(t)
 
 	runner := migration.SetupMigrationRunner{
