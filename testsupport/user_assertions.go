618b9d68e01c51581da4aad013da53420f984710
Baiju Muthukadan <baiju.m.mail@gmail.com>
2024-01-03 17:35:50 +0530

    verify  HomeSpace in UserSignup status (#829)

--- testsupport/user_assertions.go
+++ testsupport/user_assertions.go
@@ -267,6 +267,7 @@ func VerifySpaceRelatedResources(t *testing.T, awaitilities wait.Awaitilities, u
 	require.NoError(t, err)
 	tiers.VerifyNSTemplateSet(t, hostAwait, memberAwait, nsTemplateSet, tierChecks)
 
+	require.Equal(t, space.Name, userSignup.Status.HomeSpace)
 	return space
 }
 
