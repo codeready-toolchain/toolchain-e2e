85f757a84d7f811b7e8c531e886ab455fe9ca99b
Feny Mehta <fbm3307@gmail.com>
2024-06-26 16:22:31 +0530

    add compile
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/verify-dependencies.mk
+++ make/verify-dependencies.mk
@@ -1,6 +1,6 @@
 .PHONY: verify-dependencies
 ## Runs commands to verify after the updated dependecies of toolchain-common/API(go mod replace), if the repo needs any changes to be made
-verify-dependencies: tidy vet test lint-go-code
+verify-dependencies: tidy vet compile test lint-go-code
 
 .PHONY: tidy
 tidy: 
@@ -9,4 +9,8 @@ tidy:
 .PHONY: vet
 vet:
 	go vet ./...
+
+.PHONY: compile
+compile:
+	go test ./... -skip '.*'
 	
