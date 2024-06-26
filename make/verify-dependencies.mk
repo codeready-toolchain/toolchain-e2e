a6f6a8f90c8a1877fb75997c71867f24df3e709c
Feny Mehta <fbm3307@gmail.com>
2024-06-26 12:48:49 +0530

    KUBESAW-124: Makefile target for dependency validations
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

---
+++ make/verify-dependencies.mk
@@ -0,0 +1,12 @@
+.PHONY: verify-dependencies
+## Runs commands to verify after the updated dependecies of toolchain-common/API(go mod replace), if the repo needs any changes to be made
+verify-dependencies: tidy vet test lint
+
+.PHONY: tidy
+tidy: 
+	go mod tidy
+
+.PHONY: vet
+vet:
+	go vet ./...
+	
