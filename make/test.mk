46275c7cfc8c6597257c90e86e398cb06eadd65d
Feny Mehta <fbm3307@gmail.com>
2024-08-01 15:20:19 +0530

    insecure flag
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/test.mk
+++ make/test.mk
@@ -30,9 +30,13 @@ endif
 
 E2E_TEST_EXECUTION ?= true
 
+LETS_ENCRYPT_PARAM_VALUE :=  false
+
 ifeq ($(IS_OSD),true)
-LETS_ENCRYPT_PARAM := --lets-encrypt
+LETS_ENCRYPT_PARAM := 
+LETS_ENCRYPT_PARAM_VALUE = true
 endif
+
 
 E2E_PARALLELISM=1
 
@@ -261,8 +265,8 @@ print-operator-logs:
 
 .PHONY: setup-toolchainclusters
 setup-toolchainclusters: ksctl
-	${BIN_DIR}/ksctl adm register-member --host-ns $(HOST_NS) --member-ns $(MEMBER_NS) --host-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --member-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) LETS_ENCRYPT_PARAM=${LETS_ENCRYPT_PARAM}
-	if [[ ${SECOND_MEMBER_MODE} == true ]]; then ${BIN_DIR}/ksctl adm register-member --host-ns $(HOST_NS) --member-ns $(MEMBER_NS_2) --host-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --member-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) LETS_ENCRYPT_PARAM=${LETS_ENCRYPT_PARAM}; fi
+	${BIN_DIR}/ksctl adm register-member --host-ns $(HOST_NS) --member-ns $(MEMBER_NS) --host-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --member-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --lets-encrypt ${LETS_ENCRYPT_PARAM_VALUE}
+	if [[ ${SECOND_MEMBER_MODE} == true ]]; then ${BIN_DIR}/ksctl adm register-member --host-ns $(HOST_NS) --member-ns $(MEMBER_NS_2) --host-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --member-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --lets-encrypt ${LETS_ENCRYPT_PARAM_VALUE} ; fi
 	echo "Restart host operator pods so it can get the ToolchainCluster CRs while it's starting up".
 	oc delete pods --namespace ${HOST_NS} -l control-plane=controller-manager
 
