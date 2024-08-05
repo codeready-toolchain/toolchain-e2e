dd04e4d0ec8217f7a298ddf6c4f4faed2fa09aee
Feny Mehta <fbm3307@gmail.com>
2024-08-05 16:48:39 +0530

    rc
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/test.mk
+++ make/test.mk
@@ -30,10 +30,9 @@ endif
 
 E2E_TEST_EXECUTION ?= true
 
-LETS_ENCRYPT_PARAM_VALUE := false
 
 ifeq ($(IS_OSD),true)
-LETS_ENCRYPT_PARAM := --lets-encrypt
+LETS_ENCRYPT_PARAM := --lets-encrypt=false
 endif
 
 
@@ -264,7 +263,7 @@ print-operator-logs:
 
 .PHONY: setup-toolchainclusters
 setup-toolchainclusters: ksctl
-	${BIN_DIR}/ksctl adm register-member --host-ns $(HOST_NS) --member-ns $(MEMBER_NS) --host-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --member-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --lets-encrypt=${LETS_ENCRYPT_PARAM_VALUE}
+	${BIN_DIR}/ksctl adm register-member --host-ns="$(HOST_NS)" --member-ns="$(MEMBER_NS)" --host-kubeconfig="$(or ${KUBECONFIG}, ${HOME}/.kube/config)" --member-kubeconfig="$(or ${KUBECONFIG}, ${HOME}/.kube/config)" --lets-encrypt="${LETS_ENCRYPT_PARAM_VALUE}"
 	if [[ ${SECOND_MEMBER_MODE} == true ]]; then ${BIN_DIR}/ksctl adm register-member --host-ns $(HOST_NS) --member-ns $(MEMBER_NS_2) --host-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --member-kubeconfig $(or ${KUBECONFIG}, ${HOME}/.kube/config) --lets-encrypt=${LETS_ENCRYPT_PARAM_VALUE} ; fi
 	echo "Restart host operator pods so it can get the ToolchainCluster CRs while it's starting up".
 	oc delete pods --namespace ${HOST_NS} -l control-plane=controller-manager
