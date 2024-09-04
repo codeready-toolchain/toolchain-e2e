6a7f9f1341a0ca41e994aa64098b702edd3da4b4
Kanika Rana <krana@redhat.com>
2024-09-04 00:17:00 -0400

    use context error instead

--- testsupport/wait/host.go
+++ testsupport/wait/host.go
@@ -889,7 +889,7 @@ func (a *HostAwaitility) CheckMasterUserRecordIsDeleted(t *testing.T, name strin
 		}
 		return false, fmt.Errorf("the MasterUserRecord '%s' should not be present, but it is", name)
 	})
-	require.Equal(t, wait.ErrorInterrupted(goerr.New("context deadline exceeded")), err)
+	require.Equal(t, context.DeadlineExceeded, err)
 }
 
 func containsUserAccountStatus(uaStatuses []toolchainv1alpha1.UserAccountStatusEmbedded, uaStatus toolchainv1alpha1.UserAccountStatusEmbedded) bool {
@@ -1277,7 +1277,7 @@ func (a *HostAwaitility) WaitForNotificationToNotBeCreated(t *testing.T, notific
 	if err == nil {
 		return fmt.Errorf("notification '%s' was found, but it was expected to not be created/present: \n %v", notificationName, notification)
 	}
-	if goerr.Is(err, wait.ErrorInterrupted(goerr.New("context deadline exceeded"))) {
+	if goerr.Is(err, context.DeadlineExceeded) {
 		return nil
 	}
 	return err
