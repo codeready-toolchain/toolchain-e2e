14f4c0f0f5bbdbbb87e4cf988a8d20e9b8a40f6f
Kanika Rana <krana@redhat.com>
2024-10-03 22:09:49 -0400

    remove comment which is no longer valid, and update comment msg

--- test/e2e/parallel/proxy_test.go
+++ test/e2e/parallel/proxy_test.go
@@ -364,15 +364,10 @@ func TestProxyFlow(t *testing.T) {
 				require.NoError(t, err)
 				err = proxyCl.Create(context.TODO(), appToCreate)
 
-				// then
-				// note: the actual error message is "invalid workspace request: access to workspace '%s' is forbidden" but clients make api discovery calls
-				// that fail because in this case the api discovery calls go through the proxyWorkspaceURL which is invalid. If using oc or kubectl and you
-				// enable verbose logging you would see Response Body: invalid workspace request: access to workspace 'proxymember2' is forbidden
-
 				// Sep 26 2024
-				// As mentioned in the issue -https://github.com/kubernetes-sigs/controller-runtime/issues/2354 in introduced in controller-runtime v0.15
-				// for this test, instead of expecting the NoMatchError, discovery client fails and returns a server list error 'failed to get API group resources: unable to retrieve the complete list of server APIs'
-				// But since the returned error is wrapped around the actual error message mentioned in the above comments, updated to check error contains nstead of error eqals
+				// As mentioned in the issue -https://github.com/kubernetes-sigs/controller-runtime/issues/2354 introduced in controller-runtime v0.15
+				// for this test, instead of expecting the NoMatchError, discovery client fails and returns a server list error like 'failed to get API group resources: unable to retrieve the complete list of server APIs'
+				// The returned server error wraps around the actual error message of ""invalid workspace request", thus check error contains instead of error equals
 				// TO-DO : This issue is fixed in controller-runtime v0.17 - revisit it then
 				require.ErrorContains(t, err, fmt.Sprintf("invalid workspace request: access to workspace '%s' is forbidden", workspaceName))
 			})
