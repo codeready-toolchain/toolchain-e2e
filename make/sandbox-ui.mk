4b75d4b9a0b55c0f216df92111c0332c7fd1f23a
Rafaela Soares <rsoaresd@redhat.com>
2025-05-29 14:19:36 +0100

    switch to quay

--- make/sandbox-ui.mk
+++ make/sandbox-ui.mk
@@ -4,6 +4,7 @@ TAG := latest
 PLATFORM = linux/amd64
 RHDH_PLUGINS_DIR = $(TMPDIR)rhdh-plugins
 AUTH_FILE := /tmp/auth.json
+IMAGE_TO_PUSH_IN_QUAY ?= quay.io/$(QUAY_NAMESPACE)/sandbox-rhdh-plugin:$(TAG)
 
 .PHONY: deploy-sandbox-ui
 deploy-sandbox-ui: REGISTRATION_SERVICE_API=https://$(shell oc get route registration-service -n ${HOST_NS} -o custom-columns=":spec.host" | tr -d '\n')/api/v1
@@ -29,12 +30,15 @@ deploy-sandbox-ui:
 			HOST_NS=${HOST_NS} \
 			HOST_OPERATOR_API=${HOST_OPERATOR_API} \
 			SANDBOX_UI_NS=${SANDBOX_UI_NS} \
-			SANDBOX_PLUGIN_IMAGE=${OS_IMAGE_REGISTRY}/${SANDBOX_UI_NS}/${SANDBOX_PLUGIN_IMAGE_NAME}:${TAG} \
+			SANDBOX_PLUGIN_IMAGE=${IMAGE_TO_PUSH_IN_QUAY} \
 			RHDH=${RHDH} envsubst | oc apply -f -
 	@echo "restarting registration-service to apply toolchainconfig changes"
 	@oc -n ${HOST_NS} rollout restart deploy/registration-service
 	@oc -n ${SANDBOX_UI_NS} rollout status deploy/rhdh
 	@echo "Developer Sandbox UI running at ${RHDH}"
+
+
+# SANDBOX_PLUGIN_IMAGE=${OS_IMAGE_REGISTRY}/${SANDBOX_UI_NS}/${SANDBOX_PLUGIN_IMAGE_NAME}:${TAG} \
 
 
 .PHONY: deploy-sandbox-ui-local
@@ -66,10 +70,24 @@ clone-rhdh-plugins:
 	git clone https://github.com/redhat-developer/rhdh-plugins $(RHDH_PLUGINS_DIR) && \
 	echo "cloned to $(RHDH_PLUGINS_DIR)"
 
+# .PHONY: push-sandbox-plugin
+# push-sandbox-plugin: check-registry
+# push-sandbox-plugin: OS_IMAGE_REGISTRY=$(shell oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}' 2>/dev/null || true)
+# push-sandbox-plugin: IMAGE_TO_PUSH=${OS_IMAGE_REGISTRY}/${SANDBOX_UI_NS}/${SANDBOX_PLUGIN_IMAGE_NAME}:${TAG}
+# push-sandbox-plugin:
+# 	$(MAKE) get-rhdh-plugins
+# 	cd $(RHDH_PLUGINS_DIR)/workspaces/sandbox && \
+# 	rm -rf plugins/sandbox/dist-dynamic && \
+# 	rm -rf red-hat-developer-hub-backstage-plugin-sandbox && \
+# 	yarn install && \
+# 	npx @janus-idp/cli@3.3.1 package package-dynamic-plugins \
+# 		--tag ${IMAGE_TO_PUSH} \
+# 		--platform ${PLATFORM} && \
+# 	echo "podman push ${IMAGE_TO_PUSH} --creds=${OC_WHOAMI}:${OC_WHOAMI_TOKEN} --tls-verify=false" && \
+# 	podman push ${IMAGE_TO_PUSH} --creds=${OC_WHOAMI}:${OC_WHOAMI_TOKEN} --tls-verify=false
+
+
 .PHONY: push-sandbox-plugin
-push-sandbox-plugin: check-registry
-push-sandbox-plugin: OS_IMAGE_REGISTRY=$(shell oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}' 2>/dev/null || true)
-push-sandbox-plugin: IMAGE_TO_PUSH=${OS_IMAGE_REGISTRY}/${SANDBOX_UI_NS}/${SANDBOX_PLUGIN_IMAGE_NAME}:${TAG}
 push-sandbox-plugin:
 	$(MAKE) get-rhdh-plugins
 	cd $(RHDH_PLUGINS_DIR)/workspaces/sandbox && \
@@ -77,7 +95,6 @@ push-sandbox-plugin:
 	rm -rf red-hat-developer-hub-backstage-plugin-sandbox && \
 	yarn install && \
 	npx @janus-idp/cli@3.3.1 package package-dynamic-plugins \
-		--tag ${IMAGE_TO_PUSH} \
-		--platform ${PLATFORM} && \
-	echo "podman push ${IMAGE_TO_PUSH} --creds=${OC_WHOAMI}:${OC_WHOAMI_TOKEN} --tls-verify=false" && \
-	podman push ${IMAGE_TO_PUSH} --creds=${OC_WHOAMI}:${OC_WHOAMI_TOKEN} --tls-verify=false
+      --tag $(IMAGE_TO_PUSH_IN_QUAY) \
+      --platform $(PLATFORM) && \
+  podman push $(IMAGE_TO_PUSH_IN_QUAY)
