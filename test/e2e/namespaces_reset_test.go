f568da124e7e78bb619621839b8c2805f7f97e11
MikelAlejoBR <mbarcina@redhat.com>
2026-02-16 11:31:57 -0500

    feature: tests for the "reset namespaces" feature
    These tests verify that the "reset namespaces" endpoint from the
    registration service works as expected.
    
    SANDBOX-1067

---
+++ test/e2e/namespaces_reset_test.go
@@ -0,0 +1,120 @@
+package e2e
+
+import (
+	"crypto/tls"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	commonauth "github.com/codeready-toolchain/toolchain-common/pkg/test/auth"
+	"github.com/codeready-toolchain/toolchain-e2e/testsupport"
+	authsupport "github.com/codeready-toolchain/toolchain-e2e/testsupport/auth"
+	"github.com/codeready-toolchain/toolchain-e2e/testsupport/space"
+	"github.com/codeready-toolchain/toolchain-e2e/testsupport/wait"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type namespaceResetFeatureIntegrationSuite struct {
+	suite.Suite
+	wait.Awaitilities
+	httpClient http.Client
+}
+
+func (s *namespaceResetFeatureIntegrationSuite) SetupSuite() {
+	s.Awaitilities = testsupport.WaitForDeployments(s.T())
+	s.httpClient = http.Client{
+		Timeout: 3 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true, // nolint:gosec
+			},
+		},
+	}
+}
+
+func TestNamespacesResetE2E(t *testing.T) {
+	suite.Run(t, &namespaceResetFeatureIntegrationSuite{})
+}
+
+// TestResetNamespaces tests that the "namespace reset" endpoint works as
+// expected.
+func (s *namespaceResetFeatureIntegrationSuite) TestResetNamespaces() {
+	// given
+	memberAwaitily := s.Member1()
+
+	// Create a new user signup for the test.
+	userOne := testsupport.NewSignupRequest(s.Awaitilities).
+		Username("userone").
+		Email("userone@redhat.com").
+		ManuallyApprove().
+		EnsureMUR().
+		TargetCluster(s.Member1()).
+		RequireConditions(wait.ConditionSet(wait.Default(), wait.ApprovedByAdmin())...).
+		Execute(s.T())
+
+	// Get the user's "NSTemplateSet" and its provisioned namespaces for the
+	// later checks.
+	_, nsTemplateSet := space.VerifyResourcesProvisionedForSpace(s.T(), s.Awaitilities, userOne.Space.Name)
+	provisionedNamespaces := nsTemplateSet.Status.ProvisionedNamespaces
+
+	// Create a token and identity to invoke the "reset-namespace" with.
+	userUuid, err := uuid.NewUUID()
+	if err != nil {
+		s.FailNow("unable to generate user UUID", err)
+	}
+
+	userIdentity := &commonauth.Identity{
+		ID:       userUuid,
+		Username: userOne.UserSignup.Status.CompliantUsername,
+	}
+	claims := []commonauth.ExtraClaim{commonauth.WithEmailClaim(userOne.UserSignup.Spec.IdentityClaims.Email)}
+	claims = append(claims, commonauth.WithUserIDClaim("111222"))
+	claims = append(claims, commonauth.WithAccountIDClaim("999111"))
+	claims = append(claims, commonauth.WithGivenNameClaim("John"))
+	claims = append(claims, commonauth.WithFamilyNameClaim("Doe"))
+	claims = append(claims, commonauth.WithCompanyClaim("Red Hat"))
+	claims = append(claims, commonauth.WithAccountNumberClaim("2222"))
+
+	token, err := authsupport.NewTokenFromIdentity(userIdentity, claims...)
+	require.NoError(s.T(), err, `ùnable to create the token for the "namespace reset" request`)
+
+	// when
+	// Call the endpoint under test.
+	req, err := http.NewRequest(http.MethodPost, s.Host().RegistrationServiceURL+"/api/v1/reset-namespaces", nil)
+	require.NoError(s.T(), err, `unable to create the "namespace reset" request`)
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := s.httpClient.Do(req)
+	require.NoError(s.T(), err, `unable to send the "namespace reset" request`)
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			require.NoError(s.T(), err, `unable to close the "namespace reset" response's' body`)
+		}
+	}()
+
+	bodyRaw, err := io.ReadAll(resp.Body)
+	require.NoError(s.T(), err, `unable to read the response body for the "namespace reset" request`)
+	require.Equal(s.T(), http.StatusAccepted, resp.StatusCode, "unexpected status code received. Response body: %s", string(bodyRaw))
+
+	// then
+	// Wait for the user's namespaces to reach the "terminating" status.
+	for _, pns := range provisionedNamespaces {
+		_, err := memberAwaitily.WaitForNamespaceInTerminating(s.T(), pns.Name)
+		require.NoError(s.T(), err, `unexpected error when waiting for the namespace "%s" to be terminated`, pns.Name)
+	}
+
+	// Verify that the namespaces are provisioned again...
+	nsTmplSet, err := memberAwaitily.WaitForNSTmplSet(s.T(), userOne.UserSignup.Status.CompliantUsername, wait.UntilNSTemplateSetHasConditions(wait.Provisioned()))
+	require.NoError(s.T(), err, `unexpected error when waiting for the "NSTemplateSet" to become "provisioned"`)
+
+	// ... and double check that the namespaces are active.
+	for _, namespace := range nsTmplSet.Spec.Namespaces {
+		_, err := memberAwaitily.WaitForNamespace(s.T(), userOne.UserSignup.Status.CompliantUsername, namespace.TemplateRef, nsTmplSet.Spec.TierName, wait.UntilNamespaceIsActive())
+		require.NoError(s.T(), err, `unexpected error when waiting for the namespace "%s" to become active`, namespace.TemplateRef)
+	}
+}
